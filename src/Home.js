570fc8f8d392fa0be010da609882e581ab12ab20
paras-017 <paraschauhan0017@gmail.com>
2023-07-16 16:39:11 +0530

    V8 sevices and trusted section complete

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted";
 
 const Home = () => {
   const data={
     name:"Thapa Store"
   }
-  return <HeroSection myData={data}/>
+  return(
+    <>
+     <HeroSection myData={data}/>
+     <Services/>
+     <Trusted/>
+    </>
+  )
 };
 
 const Wrapper = styled.section`
