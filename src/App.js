284b14e9bc58f86a93a406c3d34815badf70ff37
paras-017 <paraschauhan0017@gmail.com>
2023-07-16 09:47:06 +0530

    V5 header component and navbar created

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Cart from "./Cart";
 import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 
 const App = () => {
   const theme = {
@@ -38,7 +39,8 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-      <GlobalStyle/>
+        <GlobalStyle/>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
