f420bfa1ccd4d6f5b84129ab94c450bf7b250445
paras-017 <paraschauhan0017@gmail.com>
2023-07-19 10:42:20 +0530

    v#31 do-->sorting section with optimized code

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -4,7 +4,7 @@ import { BsFillGridFill, BsList } from "react-icons/bs";
 import { useFilterContext } from "../context/filter_context";
 
 const Sort = () => {
-  const { filter_products, grid_view, setGridView, setListView, sorting } =useFilterContext();
+  const {sorting, filter_products, grid_view, setGridView, setListView  } =useFilterContext();
   return (
     <Wrapper className="sort-section">
       {/* 1st column  */}
